7d2bee0362d22ce79e1c72dbe8ec5e90d26b90e2
ilyamalgin <soundmaker1505@gmail.com>
2018-08-08 01:42:11 +0300

    Debug output without duplicates + some additional info

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -4,48 +4,52 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/vec3.hpp>
+#include <unordered_set>
 #include "game/Game.h"
 #include "game/Options.h"
 #include "graphics/Camera.h"
 
+std::unordered_set<GLuint> debugMessages;
 void APIENTRY glDebugCallback(GLenum source,
                             GLenum type,
                             GLuint id,
                             GLenum severity,
                             GLsizei,
-                            const GLchar* message,
+                            const GLchar* glMessage,
                             const void*)
 {
-  if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
-  std::cout << "Debug message: (" << id << "): " << message << std::endl;
+  if (id == 131169 || id == 131185 || id == 131218 || id == 131204 || debugMessages.find(id) != debugMessages.end()) return;
+  debugMessages.emplace(id);
+  std::string message = "Debug message: (" + std::to_string(id) + "), ";
   switch(source)
     {
-    case GL_DEBUG_SOURCE_API: std::cout << "Source: API\n"; break;
-    case GL_DEBUG_SOURCE_WINDOW_SYSTEM: std::cout << "Source: Window System\n"; break;
-    case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler\n"; break;
-    case GL_DEBUG_SOURCE_THIRD_PARTY: std::cout << "Source: Third Party\n"; break;
-    case GL_DEBUG_SOURCE_APPLICATION: std::cout << "Source: Application\n"; break;
-    case GL_DEBUG_SOURCE_OTHER: std::cout << "Source: Other\n"; break;
+    case GL_DEBUG_SOURCE_API:             message.append("Source: API, "); break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   message.append("Source: Window System, "); break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER: message.append("Source: Shader Compiler, "); break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY:     message.append("Source: Third Party, "); break;
+    case GL_DEBUG_SOURCE_APPLICATION:     message.append("Source: Application, "); break;
+    case GL_DEBUG_SOURCE_OTHER:           message.append("Source: Other, "); break;
     }
   switch(type)
     {
-    case GL_DEBUG_TYPE_ERROR: std::cout << "Type: Error\n"; break;
-    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated behaviour\n"; break;
-    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: std::cout << "Type: Undefined behaviour\n"; break;
-    case GL_DEBUG_TYPE_PORTABILITY: std::cout << "Type: Portability\n"; break;
-    case GL_DEBUG_TYPE_PERFORMANCE: std::cout << "Type: Performance\n"; break;
-    case GL_DEBUG_TYPE_MARKER: std::cout << "Type: Marker\n"; break;
-    case GL_DEBUG_TYPE_PUSH_GROUP: std::cout << "Type: Push Group\n"; break;
-    case GL_DEBUG_TYPE_POP_GROUP: std::cout << "Type: Pop Group\n"; break;
-    case GL_DEBUG_TYPE_OTHER: std::cout << "Type: Other\n"; break;
+    case GL_DEBUG_TYPE_ERROR:               message.append("Type: Error, "); break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: message.append("Type: Deprecated behaviour, "); break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  message.append("Type: Undefined behaviour, "); break;
+    case GL_DEBUG_TYPE_PORTABILITY:         message.append("Type: Portability, "); break;
+    case GL_DEBUG_TYPE_PERFORMANCE:         message.append("Type: Performance, "); break;
+    case GL_DEBUG_TYPE_MARKER:              message.append("Type: Marker, "); break;
+    case GL_DEBUG_TYPE_PUSH_GROUP:          message.append("Type: Push Group, "); break;
+    case GL_DEBUG_TYPE_POP_GROUP:           message.append("Type: Pop Group, "); break;
+    case GL_DEBUG_TYPE_OTHER:               message.append("Type: Other, "); break;
     }
   switch(severity)
     {
-    case GL_DEBUG_SEVERITY_HIGH: std::cout << "Severity: High\n\n"; break;
-    case GL_DEBUG_SEVERITY_MEDIUM: std::cout << "Severity: Medium\n\n"; break;
-    case GL_DEBUG_SEVERITY_LOW: std::cout << "Severity: Low\n\n"; break;
-    case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: Notification\n\n"; break;
+    case GL_DEBUG_SEVERITY_HIGH:          message.append("Severity: High"); break;
+    case GL_DEBUG_SEVERITY_MEDIUM:        message.append("Severity: Medium"); break;
+    case GL_DEBUG_SEVERITY_LOW:           message.append("Severity: Low"); break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION:  message.append("Severity: Notification"); break;
     }
+  std::cout << message << '\n' << glMessage << "\n\n";
 }
 
 int scr_width;
