6c8c089342340415063fa69a38dbecb13b1d2fd9
ilyamalgin <soundmaker1505@gmail.com>
2018-11-21 20:37:54 +0300

    shadow casting light direction twist bugfix

--- src/graphics/ShadowVolume.cpp
+++ src/graphics/ShadowVolume.cpp
@@ -9,7 +9,12 @@ void ShadowVolume::update()
 {
   glm::vec3 currentPosition = sun.getCurrentPosition();
   lightDirTo = glm::normalize(glm::vec3(0.0f) - currentPosition);
-  glm::mat4 lightProjection = glm::ortho(LEFT_PLANE, RIGHT_PLANE, bottomPlane, topPlane, nearPlane, farPlane);
+  bool isPosX = currentPosition.x > 0.0f;
+  glm::mat4 lightProjection = glm::ortho(isPosX ? RIGHT_PLANE : LEFT_PLANE,
+                                         isPosX ? LEFT_PLANE : RIGHT_PLANE,
+                                         isPosX ? topPlane : bottomPlane,
+                                         isPosX ? bottomPlane : topPlane,
+                                         nearPlane, farPlane);
   glm::mat4 lightView = glm::lookAt(currentPosition, currentPosition + lightDirTo, glm::vec3(0.0f, 1.0f, 0.0f));
   lightSpaceMatrix = lightProjection * lightView;
 }
