55fe7a304f95edf7497dbcfcb87cc1995e47e32b
ilyamalgin <soundmaker1505@gmail.com>
2018-12-09 14:04:14 +0300

    ellipse trajectory for light sources, almost minimum shadow volume bounds calculation

--- src/graphics/ShadowVolume.cpp
+++ src/graphics/ShadowVolume.cpp
@@ -59,26 +59,58 @@ void ShadowVolume::updateLightSpaceMatrix(const Frustum &frustum, int layer, flo
   float boxMidZ = (boxMaxZ + boxMinZ) * 0.5f;
 
   //step 4 - calculate light source position
-  glm::vec3 lightSource(boxMidX + sunAbsPositionX * (boxWidth * 0.5f),
-                        sunAbsPositionY * (boxWidth * 0.5f),
+  const float EXPECTED_MAX_HEIGHT = 14.0f;
+  float x = boxWidth * 0.5f;
+  float angleRad = glm::atan(EXPECTED_MAX_HEIGHT / x);
+  float ellipseA = x / glm::cos(angleRad);
+  float ellipseB = EXPECTED_MAX_HEIGHT / glm::sin(angleRad);
+
+  glm::vec3 lightSource(boxMidX + sunAbsPositionX * ellipseA,
+                        sunAbsPositionY * ellipseB,
                         boxMidZ);
 
   //step 5 - calculate frustum borders
-  float frustumRight = glm::abs(boxMinZ - boxMidZ);
-  float frustumLeft = -frustumRight;
-  float frustumTop = glm::abs(boxMinX - boxMidX);
-  float frustumBottom = -frustumTop;
-  float frustumNear = 0.1f;
-  float frustumFar = boxWidth;
+  const float RIGHT_SIDE = glm::abs(boxMinZ - boxMidZ);
+  const float LEFT_SIDE = -RIGHT_SIDE;
+
+  glm::vec2 farPoint(lightDirTo.x > 0.0f ? boxMaxX : boxMinX, 0.0f);
+  glm::vec2 fromLStoFarPoint = farPoint - glm::vec2(lightSource);
+  float fromLStoFarPointLength = glm::length(fromLStoFarPoint);
+  glm::vec2 fromLStoFarPointNorm = glm::normalize(fromLStoFarPoint);
+  float cosAlpha = glm::dot(fromLStoFarPointNorm, glm::vec2(lightDirTo));
+  const float FAR_SIDE = fromLStoFarPointLength * cosAlpha
+                          + 4.0f * sunAbsPositionY; //+some offset when the sun is at its zenith
+
+  glm::vec2 nearPoint(lightDirTo.x > 0.0f ? boxMinX : boxMaxX, EXPECTED_MAX_HEIGHT);
+  glm::vec2 fromLStoNearPoint = nearPoint - glm::vec2(lightSource);
+  float fromLStoNearPointLength = glm::length(fromLStoNearPoint);
+  glm::vec2 fromLStoNearPointNorm = glm::normalize(fromLStoNearPoint);
+  cosAlpha = glm::dot(fromLStoNearPointNorm, glm::vec2(lightDirTo));
+  const float NEAR_SIDE = fromLStoNearPointLength * cosAlpha;
+
+  glm::vec2 upPoint(lightDirTo.x > 0.0f ? boxMaxX : boxMinX, EXPECTED_MAX_HEIGHT);
+  glm::vec2 fromLStoUpPoint = upPoint - glm::vec2(lightSource);
+  float fromLStoUpPointLength = glm::length(fromLStoUpPoint);
+  glm::vec2 fromLStoUpPointNorm = glm::normalize(fromLStoUpPoint);
+  cosAlpha = glm::dot(fromLStoUpPointNorm, glm::vec2(lightDirUp));
+  const float UP_SIDE = fromLStoUpPointLength * cosAlpha;
+
+  glm::vec2 bottomPoint(lightDirTo.x > 0.0f ? boxMinX : boxMaxX, 0.0f);
+  glm::vec2 fromLStoBottomPoint = bottomPoint - glm::vec2(lightSource);
+  float fromLStoBottomPointLength = glm::length(fromLStoBottomPoint);
+  glm::vec2 fromLStoBottomPointNorm = glm::normalize(fromLStoBottomPoint);
+  cosAlpha = glm::dot(fromLStoBottomPointNorm, glm::vec2(-lightDirUp));
+  const float BOTTOM_SIDE = -fromLStoBottomPointLength * cosAlpha
+                            - 2.0f * (1.0f - sunAbsPositionY); //+some offset when the dusk or the dawn
 
   //step 6 - calculate light space matrix
   bool isLightDirPosX = lightDirTo.x > 0.0f;
-  glm::mat4 projection = glm::ortho(!isLightDirPosX ? frustumRight : frustumLeft,
-                                    !isLightDirPosX ? frustumLeft : frustumRight,
-                                    !isLightDirPosX ? frustumTop : frustumBottom,
-                                    !isLightDirPosX ? frustumBottom : frustumTop,
-                                    frustumNear,
-                                    frustumFar);
+  glm::mat4 projection = glm::ortho(!isLightDirPosX ? RIGHT_SIDE : LEFT_SIDE,
+                                    !isLightDirPosX ? LEFT_SIDE : RIGHT_SIDE,
+                                    !isLightDirPosX ? UP_SIDE : BOTTOM_SIDE,
+                                    !isLightDirPosX ? BOTTOM_SIDE : UP_SIDE,
+                                    NEAR_SIDE,
+                                    FAR_SIDE);
   glm::mat4 view = glm::lookAt(lightSource, lightSource + lightDirTo, glm::vec3(0.0f, 1.0f, 0.0f));
   lightSpaceMatrices[layer] = projection * view;
 
@@ -88,12 +120,12 @@ void ShadowVolume::updateLightSpaceMatrix(const Frustum &frustum, int layer, flo
   shadowBoxes[layer].ur = glm::vec2(boxMaxX, boxMinZ);
   shadowBoxes[layer].ul = glm::vec2(boxMinX, boxMinZ);
   shadowBoxes[layer].lightSource = lightSource;
-  shadowBoxes[layer].nearLL = lightSource + (frustumNear * lightDirTo) + (frustumLeft * lightDirRight)  + (frustumBottom * lightDirUp);
-  shadowBoxes[layer].nearLR = lightSource + (frustumNear * lightDirTo) + (frustumRight * lightDirRight) + (frustumBottom * lightDirUp);
-  shadowBoxes[layer].nearUR = lightSource + (frustumNear * lightDirTo) + (frustumRight * lightDirRight) + (frustumTop * lightDirUp);
-  shadowBoxes[layer].nearUL = lightSource + (frustumNear * lightDirTo) + (frustumLeft * lightDirRight)  + (frustumTop * lightDirUp);
-  shadowBoxes[layer].farLL =  lightSource + (frustumFar * lightDirTo)  + (frustumLeft * lightDirRight)  + (frustumBottom * lightDirUp);
-  shadowBoxes[layer].farLR =  lightSource + (frustumFar * lightDirTo)  + (frustumRight * lightDirRight) + (frustumBottom * lightDirUp);
-  shadowBoxes[layer].farUR =  lightSource + (frustumFar * lightDirTo)  + (frustumRight * lightDirRight) + (frustumTop * lightDirUp);
-  shadowBoxes[layer].farUL =  lightSource + (frustumFar * lightDirTo)  + (frustumLeft * lightDirRight)  + (frustumTop * lightDirUp);
+  shadowBoxes[layer].nearLL = lightSource + (NEAR_SIDE * lightDirTo) + (LEFT_SIDE * lightDirRight)  + (BOTTOM_SIDE * lightDirUp);
+  shadowBoxes[layer].nearLR = lightSource + (NEAR_SIDE * lightDirTo) + (RIGHT_SIDE * lightDirRight) + (BOTTOM_SIDE * lightDirUp);
+  shadowBoxes[layer].nearUR = lightSource + (NEAR_SIDE * lightDirTo) + (RIGHT_SIDE * lightDirRight) + (UP_SIDE * lightDirUp);
+  shadowBoxes[layer].nearUL = lightSource + (NEAR_SIDE * lightDirTo) + (LEFT_SIDE * lightDirRight)  + (UP_SIDE * lightDirUp);
+  shadowBoxes[layer].farLL =  lightSource + (FAR_SIDE * lightDirTo)  + (LEFT_SIDE * lightDirRight)  + (BOTTOM_SIDE * lightDirUp);
+  shadowBoxes[layer].farLR =  lightSource + (FAR_SIDE * lightDirTo)  + (RIGHT_SIDE * lightDirRight) + (BOTTOM_SIDE * lightDirUp);
+  shadowBoxes[layer].farUR =  lightSource + (FAR_SIDE * lightDirTo)  + (RIGHT_SIDE * lightDirRight) + (UP_SIDE * lightDirUp);
+  shadowBoxes[layer].farUL =  lightSource + (FAR_SIDE * lightDirTo)  + (LEFT_SIDE * lightDirRight)  + (UP_SIDE * lightDirUp);
 }
