2a5acbcf7253612cc9d47b493a753c763fea3361
ilyamalgin <soundmaker1505@gmail.com>
2019-01-28 23:03:44 +0300

    Pinky sun at dusk/dawn

--- res/shaders/theSun/theSun.fs
+++ res/shaders/theSun/theSun.fs
@@ -4,7 +4,11 @@ out vec4 o_FragColor;
 
 uniform sampler2D u_texture;
 
+in float v_PositionY;
+
 void main()
 {
-    o_FragColor = texture(u_texture, gl_PointCoord);
+    vec4 sampledDiffuse = texture(u_texture, gl_PointCoord);
+    float colorMix = clamp(v_PositionY * 10, 0.0, 1.0);
+    o_FragColor = mix(vec4(0.6, 0.05, 0.05, sampledDiffuse.a), sampledDiffuse, colorMix);
 }
--- res/shaders/theSun/theSun.vs
+++ res/shaders/theSun/theSun.vs
@@ -5,7 +5,10 @@ layout (location = 0) in vec4 i_pos;
 uniform mat4 u_model;
 uniform mat4 u_projectionView;
 
+out float v_PositionY;
+
 void main()
 {
     gl_Position = (u_projectionView * u_model * i_pos).xyww;
+    v_PositionY = u_model[0][1];
 }
