7056f888366598ce5181309a8270220be1852264
ButtonHeck <soundmaker1505@gmail.com>
2019-06-15 20:29:40 +0300

    documentation for BenchmarkTimer class

--- src/util/BenchmarkTimer.cpp
+++ src/util/BenchmarkTimer.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * BenchmarkTimer.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definition for BenchmarkTimer class
+ * @version 0.1.0
+ */
+
 #include "BenchmarkTimer"
 #include "Logger"
 #include "DirectoriesSettings"
@@ -6,12 +26,18 @@
 #include <algorithm>
 #include <iterator>
 
+//static variables definition
 std::ofstream BenchmarkTimer::perFrameLogStream, BenchmarkTimer::perAppLogStream;
 bool BenchmarkTimer::outputCreated = false;
 std::map<std::string, unsigned long> BenchmarkTimer::frameBenchmarksTimings;
 std::map<std::string, int> BenchmarkTimer::frameBenchmarksInvocations;
-std::map<std::string, float> BenchmarkTimer::applicationBenchmarks;
+std::map<std::string, float> BenchmarkTimer::applicationBenchmarksTimings;
 
+/**
+* @brief constructor of a benchmark. Responsible for register benchmark in local storage and for creating log output files
+* @param title current benchmark title. No special format requirements, but better to print class and function names
+* @param isPerFrame flag indicating whether this benchmark should be treated as repetitive and included in perFrame log
+*/
 BenchmarkTimer::BenchmarkTimer(const std::string &title, bool isPerFrame)
   :
     benchmarkTitle(title),
@@ -19,11 +45,14 @@ BenchmarkTimer::BenchmarkTimer(const std::string &title, bool isPerFrame)
 {
   if (!outputCreated)
     {
+	  //every time perFrameLog is created it is ok to override previous one (this log is more "tactical")
       perFrameLogStream.open(RES_DIR + "perFrameLog.txt", std::ios_base::ate);
       if (!perFrameLogStream)
         Logger::log("perFrameLog file not found!\n");
       perFrameLogStream << "SOME INFORMATION MIGHT BE IRRELEVANT OR EVEN INCORRECT, GREAT CARE SHOULD BE TAKEN\n";
       perFrameLogStream << "SI == Single Invocation (per second), ST == separate thread, m.p.u/a/i. == mean per update/app/invocation\n";
+
+	  //for application log it might be useful to append to see the differences among older benchmarking data (strategy-like approach)
       perAppLogStream.open(RES_DIR + "perAppLog.txt", std::ios_base::app);
       if (!perAppLogStream)
         Logger::log("perAppLog file not found!\n");
@@ -31,9 +60,11 @@ BenchmarkTimer::BenchmarkTimer(const std::string &title, bool isPerFrame)
       perAppLogStream << "SI == Single Invocation (per second), ST == separate thread, m.p.u/a/i. == mean per update/app/invocation\n";
       outputCreated = true;
     }
-  if (applicationBenchmarks.find(benchmarkTitle) == applicationBenchmarks.end())
+
+  //register a benchmark if one is being called for the first time
+  if (applicationBenchmarksTimings.find(benchmarkTitle) == applicationBenchmarksTimings.end())
     {
-      applicationBenchmarks[benchmarkTitle] = 0;
+      applicationBenchmarksTimings[benchmarkTitle] = 0;
       if (isPerFrame)
         {
           frameBenchmarksTimings[benchmarkTitle] = 0;
@@ -42,12 +73,20 @@ BenchmarkTimer::BenchmarkTimer(const std::string &title, bool isPerFrame)
     }
 }
 
+/**
+* @brief operator overloading is used when a title/perFrame data depends on a comprehensive function context
+* @param title current benchmark title. No special format requirements, but better to print class and function names
+* @param isPerFrame flag indicating whether this benchmark should be treated as repetitive and included in perFrame log
+*/
 void BenchmarkTimer::operator()(const std::string &title, bool isPerFrame)
 {
   benchmarkTitle = title;
   this->isPerFrame = isPerFrame;
 }
 
+/**
+* @brief create a timestamp when benchmark is done and update timing values of this benchmark
+*/
 BenchmarkTimer::~BenchmarkTimer()
 {
   endTimestamp = chronoClock::now();
@@ -58,9 +97,13 @@ BenchmarkTimer::~BenchmarkTimer()
       frameBenchmarksTimings[benchmarkTitle] += frameTimingResultMicros;
       frameBenchmarksInvocations[benchmarkTitle]++;
     }
-  applicationBenchmarks[benchmarkTitle] += applicationTimingResultMillis;
+  applicationBenchmarksTimings[benchmarkTitle] += applicationTimingResultMillis;
 }
 
+/**
+* @brief print all accumulated app benchmarks to output file and closes log files
+* @param updateCount number of timer updates(seconds) ticked since the app beginning.
+*/
 void BenchmarkTimer::finish(unsigned int updateCount)
 {
   perFrameLogStream.flush();
@@ -70,6 +113,10 @@ void BenchmarkTimer::finish(unsigned int updateCount)
   perAppLogStream.close();
 }
 
+/**
+* @brief prepare and format app log file for all the benchmarks
+* @param updateCount number of timer updates(seconds) ticked since the app beginning. Used for time approximation of each benchmark call
+*/
 void BenchmarkTimer::printApplicationBenchmarks(unsigned int updateCount)
 {
   //prepare char buffer of the current timestamp and format it
@@ -82,15 +129,15 @@ void BenchmarkTimer::printApplicationBenchmarks(unsigned int updateCount)
 
   //create copy of application benchmark map, but switch its key<->value in order to sort them by their values, not the titles
   std::multimap<float, std::string> applicationBenchmarksSortedByValue;
-  std::transform(applicationBenchmarks.begin(),
-                 applicationBenchmarks.end(),
+  std::transform(applicationBenchmarksTimings.begin(),
+                 applicationBenchmarksTimings.end(),
                  std::inserter(applicationBenchmarksSortedByValue, applicationBenchmarksSortedByValue.begin()),
                  [](std::pair<std::string, float> p) noexcept ->std::pair<float, std::string>
   {
     return std::pair<float, std::string>(p.second, p.first);
   });
 
-  //print in descending value order without external sorting
+  //print in descending value order without external sorting (more "timing-heavy" benchmarks printed first)
   for (auto benchmark = applicationBenchmarksSortedByValue.end(); benchmark != applicationBenchmarksSortedByValue.begin();)
     {
       --benchmark;
@@ -109,6 +156,11 @@ void BenchmarkTimer::printApplicationBenchmarks(unsigned int updateCount)
     }
 }
 
+/**
+* @brief prepare and format frame log file, then fill with all perFrame benchmarks timings
+* @param updateCount number of timer updates(seconds) ticked since the app beginning.
+* @param ups current UPS(cpu updates per second) value. Used to represent UPS for a particular timer update
+*/
 void BenchmarkTimer::printFrameBenchmarks(unsigned int updateCount, unsigned int ups)
 {
   perFrameLogStream << "--Update: "
@@ -138,6 +190,9 @@ void BenchmarkTimer::printFrameBenchmarks(unsigned int updateCount, unsigned int
   perFrameLogStream << '\n';
 }
 
+/**
+* @brief for each registered benchmark set timings and invocation count to zero. Should be used at the end of a timer update 
+*/
 void BenchmarkTimer::resetFrameBenchmarks()
 {
   for (auto timingBenchmark = frameBenchmarksTimings.begin(); timingBenchmark != frameBenchmarksTimings.end(); ++timingBenchmark)
--- src/util/BenchmarkTimer.h
+++ src/util/BenchmarkTimer.h
@@ -1,16 +1,35 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * BenchmarkTimer.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains declaration for BenchmarkTimer class
+ * @version 0.1.0
+ */
+
 #pragma once
 
 #include <fstream>
 #include <map>
 #include <chrono>
 
-/* if in debug build - BENCHMARK macro creates an instance of BenchmarkTimer
+/** if in debug build - BENCHMARK macro creates an instance of BenchmarkTimer
  * which takes timestamps in both ctor and dtor
  * and automatically update timing results in one of the maps
  * if in release build - just bypass and do nothing
  */
-#define USE_BENCHMARK 0
-#if USE_BENCHMARK
+#if 0
 	#ifdef _DEBUG
 	#define BENCHMARK(benchmarkName, perFrame) BenchmarkTimer b(benchmarkName, perFrame);
 	#else
@@ -21,6 +40,11 @@
 #endif
 #define FORCE_BENCHMARK(benchmarkName, perFrame) BenchmarkTimer b(benchmarkName, perFrame);
 
+/**
+* @brief class for creating CPU time benchmarks through BENCHMARK macro
+* Responsible for creating benchmarks on demand in dedicated parts of code. Used only in debug mode
+* @todo remove from release version of the project
+*/
 class BenchmarkTimer
 {
 public:
@@ -39,7 +63,7 @@ private:
 
   static std::ofstream perFrameLogStream, perAppLogStream;
   static bool outputCreated;
-  static std::map<std::string, float> applicationBenchmarks;
+  static std::map<std::string, float> applicationBenchmarksTimings;
   static std::map<std::string, unsigned long> frameBenchmarksTimings;
   static std::map<std::string, int> frameBenchmarksInvocations;
 
