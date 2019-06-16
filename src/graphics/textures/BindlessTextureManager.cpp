c954ca89b349651bf297d31aae79f41a9d0e3283
ButtonHeck <soundmaker1505@gmail.com>
2019-06-16 19:44:00 +0300

    documentation for BindlessTextureManager

--- src/graphics/textures/BindlessTextureManager.cpp
+++ src/graphics/textures/BindlessTextureManager.cpp
@@ -1,7 +1,35 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * BindlessTextureManager.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definition for BindlessTextureManager class and BindlessTexture struct
+ * @version 0.1.0
+ */
+
 #include "BindlessTextureManager"
 #include "Shader"
 
+//static variable definition
 vec2D_template<BindlessTexture> BindlessTextureManager::textures;
+
+/**
+* @brief create bindless texture object of a given type. Additionally reserves textures storage capacity if need
+* @param textureSamplerUniformName name of a texture used as a sampler name in a shader
+* @param textureID ID of a texture in OpenGL
+* @param textureType semantic type of a texture
+*/
 void BindlessTextureManager::emplaceBack(const std::string &textureSamplerUniformName, GLuint textureID, BINDLESS_TEXTURE_TYPE textureType)
 {
   static bool capacityReserved = false;
@@ -20,6 +48,10 @@ void BindlessTextureManager::emplaceBack(const std::string &textureSamplerUnifor
   textures[textureType].emplace_back(textureSamplerUniformName, textureID, textureHandle);
 }
 
+/**
+* @brief send command to OpenGL to make all the textures resident in memory.
+* Should be used when all textures have been loaded and before game loop begins
+*/
 void BindlessTextureManager::makeAllResident()
 {
   for (unsigned int textureTypeIndex = 0; textureTypeIndex < textures.size(); textureTypeIndex++)
@@ -29,6 +61,11 @@ void BindlessTextureManager::makeAllResident()
     }
 }
 
+/**
+* @brief set textures handles for a given type to a given shader as uniform values
+* @param shader shader that uses bindless textures of a given type
+* @param textureType semantic type of a texture. It is programmer's responsibility to make sure that given type is correct for the shader
+*/
 void BindlessTextureManager::loadToShader(Shader &shader, BINDLESS_TEXTURE_TYPE textureType)
 {
   shader.use();
@@ -36,6 +73,10 @@ void BindlessTextureManager::loadToShader(Shader &shader, BINDLESS_TEXTURE_TYPE 
     shader.setUint64(texture.samplerUniformName.c_str(), texture.handle);
 }
 
+/**
+* @brief send command to OpenGL to make all the textures non-resident in memory. Additionally sends delete command for each texture.
+* Should be used when the app is about to close
+*/
 void BindlessTextureManager::makeAllNonResident()
 {
   for (unsigned int textureTypeIndex = 0; textureTypeIndex < textures.size(); textureTypeIndex++)
@@ -48,6 +89,12 @@ void BindlessTextureManager::makeAllNonResident()
     }
 }
 
+/**
+* @brief creates a bindless texture object with all necessary params
+* @param textureSamplerUniformName name of a texture used as a sampler name in a shader
+* @param textureID ID of a texture in OpenGL
+* @param handle 64-bit handle promoted to OpenGL as a pointer to the actual residental texture  
+*/
 BindlessTexture::BindlessTexture(const std::string &textureSamplerUniformName, GLuint textureID, GLuint64 handle)
   :
     samplerUniformName(textureSamplerUniformName),
--- src/graphics/textures/BindlessTextureManager.h
+++ src/graphics/textures/BindlessTextureManager.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * BindlessTextureManager.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains declaration for BindlessTextureManager class and BindlessTexture struct
+ * @version 0.1.0
+ */
+
 #pragma once
 
 #include "TypeAliases"
@@ -13,6 +33,9 @@ enum BINDLESS_TEXTURE_TYPE
   BINDLESS_TEXTURE_NUM_TYPES
 };
 
+/**
+* @brief representation of a bindless texture
+*/
 struct BindlessTexture
 {
   BindlessTexture(const std::string& samplerUniformName, GLuint id, GLuint64 handle);
@@ -21,6 +44,10 @@ struct BindlessTexture
   GLuint64 handle;
 };
 
+/**
+* @brief manager of bindless textures in the app. Responsible for keeping all the textures in the dedicated storage,
+* sending them to shaders and for residency handling.
+*/
 class BindlessTextureManager
 {
 public:
