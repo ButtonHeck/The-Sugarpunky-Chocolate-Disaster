bce174003bd847c028d46c23991888699b889098
ButtonHeck <soundmaker1505@gmail.com>
2019-06-16 23:26:58 +0300

    documentation for BuildableFacade

--- src/game/world/terrain/buildable/BuildableFacade.cpp
+++ src/game/world/terrain/buildable/BuildableFacade.cpp
@@ -1,7 +1,32 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * BuildableFacade.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definition for BuildableFacade class
+ * @version 0.1.0
+ */
+
 #include "BuildableFacade"
 #include "BenchmarkTimer"
 #include "MouseInputManager"
 
+/**
+* @brief creates a facade for buildable and selected tiles related codebase
+* @param buildableRenderShader shader used to render buidable tiles
+* @param selectedRenderShader shader used to render selected tile
+*/
 BuildableFacade::BuildableFacade(Shader &buildableRenderShader, Shader &selectedRenderShader) noexcept
   :
     shader(buildableRenderShader, selectedRenderShader),
@@ -9,11 +34,20 @@ BuildableFacade::BuildableFacade(Shader &buildableRenderShader, Shader &selected
     renderer(generator)
 {}
 
+/**
+* @brief delegates map creation to a generator
+* @param landMap map of the land tiles
+* @param hillsMap map of the hill tiles
+*/
 void BuildableFacade::setup(const map2D_f &landMap, const map2D_f &hillsMap)
 {
   generator.setup(landMap, hillsMap);
 }
 
+/**
+* @brief launches buildable tiles rendering routine
+* @param projectionView "projection * view" matrix fed to the shader
+*/
 void BuildableFacade::drawBuildable(const glm::mat4& projectionView)
 {
   shader.updateBuildable(projectionView);
@@ -23,6 +57,11 @@ void BuildableFacade::drawBuildable(const glm::mat4& projectionView)
   }
 }
 
+/**
+* @brief launches selected tile rendering routine
+* @param projectionView "projection * view" matrix fed to the shader
+* @param mouseInput mouse input object used to pick cursor current map coordinates
+*/
 void BuildableFacade::drawSelected(const glm::mat4& projectionView, MouseInputManager& mouseInput)
 {
   if (generator.getMap()[mouseInput.getCursorWorldZ()][mouseInput.getCursorWorldX()] != 0)
@@ -34,6 +73,9 @@ void BuildableFacade::drawSelected(const glm::mat4& projectionView, MouseInputMa
     }
 }
 
+/**
+* @brief getter of the buildable map
+*/
 const map2D_f &BuildableFacade::getMap() const noexcept
 {
   return generator.getMap();
