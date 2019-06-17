9b054cd83140075437dab9b4491c2400e0b6f540
ButtonHeck <soundmaker1505@gmail.com>
2019-06-17 18:04:04 +0300

    documentation for BuildableRenderer

--- src/game/world/terrain/buildable/BuildableRenderer.cpp
+++ src/game/world/terrain/buildable/BuildableRenderer.cpp
@@ -1,11 +1,38 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * BuildableRenderer.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definition for BuildableRenderer class
+ * @version 0.1.0
+ */
+
 #include "BuildableRenderer"
 #include "BuildableGenerator"
 
+/**
+* @brief plain ctor
+* @param generator generator that holds necessary GL objects pipelines
+*/
 BuildableRenderer::BuildableRenderer(BuildableGenerator &generator) noexcept
   :
     generator(generator)
 {}
 
+/**
+* @brief sends draw command to OpenGL for buildable tiles instances
+*/
 void BuildableRenderer::renderBuildable()
 {
   generator.basicGLBuffers.bind(VAO);
@@ -14,6 +41,9 @@ void BuildableRenderer::renderBuildable()
   glDisable(GL_BLEND);
 }
 
+/**
+* @brief sends draw command to OpenGL for selected tile
+*/
 void BuildableRenderer::renderSelected()
 {
   generator.selectedBuffers.bind(VAO);
