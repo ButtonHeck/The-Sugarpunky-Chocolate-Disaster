decdf03f0e986b7c79cf07c02cc654cbbf54c4c2
ButtonHeck <soundmaker1505@gmail.com>
2019-06-17 17:46:23 +0300

    documentation for BuildableGenerator

--- src/game/world/terrain/buildable/BuildableGenerator.cpp
+++ src/game/world/terrain/buildable/BuildableGenerator.cpp
@@ -1,5 +1,29 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * BuildableGenerator.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definition for BuildableGenerator class
+ * @version 0.1.0
+ */
+
 #include "BuildableGenerator"
 
+/**
+* @brief plain ctor. Adds instance buffer object for inherited buffer collection and creates vao/vbo/ebo collection
+* for selected tile
+*/
 BuildableGenerator::BuildableGenerator() noexcept
   :
     Generator(),
@@ -9,13 +33,20 @@ BuildableGenerator::BuildableGenerator() noexcept
   basicGLBuffers.add(INSTANCE_VBO);
 }
 
+/**
+* @brief walks through the world map and marks individual tiles as buildable where applicable. 
+* Then creates tiles and loads them to OpenGL
+* @param landMap map of the land tiles 
+* @param hillsMap map of the hill tiles
+*/
 void BuildableGenerator::setup(const map2D_f &landMap, const map2D_f &hillsMap)
 {
   for (unsigned int y = UPPER_LEFT_CORNER_START_Y; y < WORLD_HEIGHT; y++)
     {
       for (unsigned int x = UPPER_LEFT_CORNER_START_X; x < WORLD_WIDTH - 1; x++)
         {
-          map[y][x] = false;
+		  //implicit cast to float! In this case its ok, as we don't need any values except "has something" or "not"
+          map[y][x] = false; 
           if (landMap[y][x] == 0 &&
               landMap[y-1][x] == 0 &&
               landMap[y-1][x+1] == 0 &&
@@ -34,6 +65,9 @@ void BuildableGenerator::setup(const map2D_f &landMap, const map2D_f &hillsMap)
   fillBufferData();
 }
 
+/**
+* @brief setup buffer collections, their layouts and buffer data into it
+*/
 void BuildableGenerator::fillBufferData()
 {
   setupAndBindBuffers(selectedBuffers);
@@ -46,6 +80,8 @@ void BuildableGenerator::fillBufferData()
       TerrainTile& tile = tiles[tileIndex];
       instancesTranslations[tileIndex] = glm::vec4(-HALF_WORLD_WIDTH + tile.mapX, 0.0f, -HALF_WORLD_HEIGHT + tile.mapY, 0.0f);
     }
+
+  //the only difference of the buildable tiles is their location, thus buffer it as per-instance data
   basicGLBuffers.bind(INSTANCE_VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec4) * tiles.size(), &instancesTranslations[0], GL_STATIC_DRAW);
   glEnableVertexAttribArray(1);
@@ -55,8 +91,13 @@ void BuildableGenerator::fillBufferData()
   BufferCollection::bindZero(VAO | VBO | EBO);
 }
 
+/**
+* @brief helper function to allocate and setup basic cell geometry for both buildable and selected tiles
+* @param buffers collection of buffers to setup
+*/
 void BuildableGenerator::setupAndBindBuffers(BufferCollection &buffers)
 {
+  //make Y slightly higher 0.0 to prevent z-fighting with land
   constexpr static GLfloat CELL_VERTICES[12] = {
        0.05f, 0.01f,  -0.05f,
        0.95f, 0.01f,  -0.05f,
--- src/game/world/terrain/buildable/BuildableGenerator.h
+++ src/game/world/terrain/buildable/BuildableGenerator.h
@@ -1,7 +1,32 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * BuildableGenerator.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains declaration for BuildableGenerator class
+ * @version 0.1.0
+ */
+
 #pragma once
 
 #include "Generator"
 
+/**
+* @brief Generator for buildable segments of the world map. Technically, it doesn't "generate" anything,
+* but rather checks which tiles of already created map are available for building on them. Additionally
+* it is holding a buffer collection for currently selected tile
+*/
 class BuildableGenerator : public Generator
 {
 public:
