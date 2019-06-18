b416f3013c7b36f6ef71b32204040d139996da86
ButtonHeck <soundmaker1505@gmail.com>
2019-06-18 12:09:15 +0300

    documentation for CoordinateSystemRenderer

--- src/graphics/gui/CoordinateSystemRenderer.cpp
+++ src/graphics/gui/CoordinateSystemRenderer.cpp
@@ -1,6 +1,30 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * CoordinateSystemRenderer.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definition for CoordinateSystemRenderer class
+ * @version 0.1.0
+ */
+
 #include "CoordinateSystemRenderer"
 #include "Shader"
 
+/**
+* @brief plain ctor. Creates data for coordinate system geometry and sets layout for OpenGL buffer collection
+* @param shader shader program used during CS rendering routine (this one includes geometry shader)
+*/
 CoordinateSystemRenderer::CoordinateSystemRenderer(Shader *shader)
   :
     basicGLBuffers(VAO | VBO),
@@ -22,6 +46,11 @@ CoordinateSystemRenderer::CoordinateSystemRenderer(Shader *shader)
   BufferCollection::bindZero(VAO | VBO);
 }
 
+/**
+* @brief updates shader variables and sends draw call to OpenGL
+* @param view 3x3 view matrix
+* @param aspectRatio aspect ratio of current viewport
+*/
 void CoordinateSystemRenderer::draw(const glm::mat3 &view, float aspectRatio)
 {
   glLineWidth(2);
