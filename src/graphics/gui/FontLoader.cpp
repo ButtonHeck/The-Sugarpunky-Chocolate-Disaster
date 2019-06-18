0975318316d74e53fcfd0a2e51a5803a2c3ab6f6
ButtonHeck <soundmaker1505@gmail.com>
2019-06-18 13:16:26 +0300

    documentation for FontLoader

--- src/graphics/gui/FontLoader.cpp
+++ src/graphics/gui/FontLoader.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * FontLoader.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definition for FontLoader class
+ * @version 0.1.0
+ */
+
 #include "FontLoader"
 #include "TextureUnits"
 #include "Logger"
@@ -6,8 +26,14 @@
 #include <fstream>
 #include <sstream>
 
+/**
+* @brief plain ctor. Loads font texture to OpenGL, parses font file to create full alphabet character by character
+* @param fontFile text file representing each character parameters such as position in the texture, its width/height etc.
+* @param textureFile image file containing symbols sheet
+*/
 FontLoader::FontLoader(const std::string &fontFile, const std::string &textureFile)
 {
+  //load font texture
   if (!ilLoadImage(textureFile.c_str()))
     Logger::log("Error loading font texture: %\n", textureFile.c_str());
   ILubyte* textureData = ilGetData();
@@ -23,6 +49,7 @@ FontLoader::FontLoader(const std::string &fontFile, const std::string &textureFi
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
+  //read font file data
   std::ifstream inputStream(fontFile);
   std::stringstream stringBuffer;
   stringBuffer << inputStream.rdbuf();
@@ -31,6 +58,7 @@ FontLoader::FontLoader(const std::string &fontFile, const std::string &textureFi
   stringBuffer >> token;
   unsigned int numberOfCharacters = std::stoi(token);
 
+  //parse string buffer and create alphabet
   for (unsigned int characterIndex = 0; characterIndex < numberOfCharacters; ++characterIndex)
     {
       Character character;
--- src/graphics/gui/FontLoader.h
+++ src/graphics/gui/FontLoader.h
@@ -1,17 +1,42 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * FontLoader.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains declaration for FontLoader class and Character struct
+ * @version 0.1.0
+ */
+
 #pragma once
 
 #include <GL/glew.h>
 #include <unordered_map>
 
+/**
+* @brief representation of a plain text symbol
+*/
 struct Character {
   int asciiCode, x, y, width, height, xoffset, yoffset, xadvance;
 };
 
+/**
+* @brief font handling class. Responsible for creating an alphabet of Character objects from given files
+*/
 class FontLoader
 {
 public:
   FontLoader(const std::string& fontFile, const std::string& fontTexture);
-  virtual ~FontLoader() = default;
   std::unordered_map<char, Character>& getAlphabet() noexcept;
   GLuint getTextureWidth() const noexcept;
   GLuint getTextureHeight() const noexcept;
