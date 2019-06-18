5c8671438bef02412bc0d0a57033934dcf8527f5
ButtonHeck <soundmaker1505@gmail.com>
2019-06-18 20:46:21 +0300

    documentation for Game

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * Game.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definition for Game class
+ * @version 0.1.0
+ */
+
 #include "Game"
 #include "BindlessTextureManager"
 #include "MouseInputManager"
@@ -6,13 +26,19 @@
 #include "RendererStateManager"
 #include "Shader"
 
+/**
+* @brief plain ctor. Creates all the submodules, sets randomizer seed
+* @param window window of the game
+* @param screenResolution current resolution of the screen
+*/
 Game::Game(GLFWwindow *window, const ScreenResolution &screenResolution)
   :
     screenResolution(screenResolution),
     window(window),
+	updateCount(0),
     camera(glm::vec3(0.0f, 12.0f, 0.0f)),
     shadowCamera(camera),
-    shadowFrustumRenderers({{shadowCameraFrustums[0], shadowCameraFrustums[1]}}),
+    shadowRegionsFrustumsRenderers({{shadowRegionsFrustums[0], shadowRegionsFrustums[1]}}),
     projection(glm::perspective(glm::radians(camera.getZoom()), screenResolution.getAspectRatio(), NEAR_PLANE, FAR_PLANE)),
     cullingProjection(glm::perspective(glm::radians(camera.getZoom() + 10.0f), screenResolution.getAspectRatio(), NEAR_PLANE, FAR_PLANE)),
     options(),
@@ -34,12 +60,21 @@ Game::Game(GLFWwindow *window, const ScreenResolution &screenResolution)
 {
   srand(time(NULL));
   Model::bindTextureLoader(textureLoader);
-  shadowProjections[0] = glm::perspective(glm::radians(camera.getZoom()), screenResolution.getAspectRatio(), NEAR_PLANE, SHADOW_DISTANCE_LAYER1);
-  shadowProjections[1] = glm::perspective(glm::radians(camera.getZoom()), screenResolution.getAspectRatio(), SHADOW_DISTANCE_LAYER1, SHADOW_DISTANCE_LAYER2);
-  shadowProjections[2] = glm::perspective(glm::radians(camera.getZoom()), screenResolution.getAspectRatio(), SHADOW_DISTANCE_LAYER2, FAR_PLANE);
-  modelsIndirectBufferPrepared = false, modelsIndirectBufferNeedUpdate = false;
+
+  //setup shadow volume projections
+  shadowRegionsProjections[0] = glm::perspective(glm::radians(camera.getZoom()), screenResolution.getAspectRatio(), NEAR_PLANE, SHADOW_DISTANCE_LAYER1);
+  shadowRegionsProjections[1] = glm::perspective(glm::radians(camera.getZoom()), screenResolution.getAspectRatio(), SHADOW_DISTANCE_LAYER1, SHADOW_DISTANCE_LAYER2);
+  shadowRegionsProjections[2] = glm::perspective(glm::radians(camera.getZoom()), screenResolution.getAspectRatio(), SHADOW_DISTANCE_LAYER2, FAR_PLANE);
+
+  //threads stuff
+  modelsIndirectBufferPrepared = false; 
+  modelsIndirectBufferNeedUpdate = false;
+  landIndirectBufferHasUpdated = false;
 }
 
+/**
+* @brief waits for coroutine threads to finish, sends finalization commands to submodules
+*/
 Game::~Game()
 {
   meshIndirectBufferUpdater->join();
@@ -47,6 +82,9 @@ Game::~Game()
   BindlessTextureManager::makeAllNonResident();
 }
 
+/**
+* @brief initializes all the submodules states, sends initialization commands to framebuffers
+*/
 void Game::setup()
 {
   BENCHMARK("Game: setup", false);
@@ -67,20 +105,34 @@ void Game::setup()
   refractionFramebuffer.setup();
 }
 
+/**
+* @brief "main" method of the game. Manages entire workflow of the game
+*/
 void Game::loop()
 {
+  /*
+  * view matrix is unique per update, thus should be recreated in each subsequent frame. 
+  * projectionView also updates once per frame (projection matrix will probably always be constant) 
+  * and then it is used in all necessary places
+  */
   glm::mat4 view, projectionView;
+
   float timerDelta = CPU_timer.tick();
+
   {
     BENCHMARK("Game loop: process input and camera", true);
     keyboard.processInput();
     camera.updateViewDirection();
     camera.move(timerDelta, scene.getHillsFacade().getMap());
+
+	//projection and view matrices
     if (!options[OPT_USE_SHADOW_CAMERA_MATRIX])
       view = camera.getViewMatrix();
     else
       view = shadowCamera.getViewMatrix();
     projectionView = projection * view;
+
+	//frustums update
     viewFrustum.updateFrustum(projectionView);
     cullingViewFrustum.updateFrustum(cullingProjection * view);
 
@@ -91,12 +143,19 @@ void Game::loop()
       }
   }
 
+  //ambience update
   scene.getSunFacade().move(timerDelta);
   scene.getSkysphereFacade().moveStarsSkysphere(timerDelta * PLANET_MOVE_SPEED);
 
+  //update sky color
   glm::vec4 currentColor = glm::mix(NIGHT_SKY_COLOR, DAY_SKY_COLOR, glm::clamp(-scene.getSunFacade().getLightDir().y * 5, 0.0f, 1.0f));
   glClearColor(currentColor.r, currentColor.g, currentColor.b, currentColor.a);
 
+  /*
+  * we might have to wait until models indirect buffer coroutine thread is done, 
+  * in case that thread is not ready yet we could update land chunks indirect buffer in parallel
+  * and then just chill till second thread is ready. Otherwise explicitly update land indirect buffer after
+  */
   landIndirectBufferHasUpdated = false;
   {
     BENCHMARK("Game loop: wait models DIBs ready", true);
@@ -115,13 +174,24 @@ void Game::loop()
   if (!landIndirectBufferHasUpdated)
     scene.getLandFacade().updateCellsIndirectBuffer(viewFrustum);
 
+  //world recreation routine
   if (options[OPT_RECREATE_TERRAIN_REQUEST])
     recreate();
 
+  /*
+  * by this time model indirect buffer updater thread has prepared data, so buffer them to GPU.
+  * This also should be done before any draw call that uses that data, even draw call to depthmap
+  */
   scene.getPlantsFacade().updateIndirectBufferData();
-  if (options[OPT_USE_SHADOWS] && updateCount % 2)
-    updateDepthmap();
 
+  //save some processing time by updating depthmap every two frames
+  if (options[OPT_USE_SHADOWS] && updateCount % 2)
+    drawDepthmap();
+
+  /*
+  * draw world reflection/refraction texture ONLY if any water was visible in PREVIOUS frame,
+  * thus we don't have to wait for occlusion query result during this frame
+  */
   if(scene.getWaterFacade().hasWaterInFrame())
     {
       reflectionFramebuffer.bindToViewport(FRAME_WATER_REFLECTION_WIDTH, FRAME_WATER_REFLECTION_HEIGHT);
@@ -131,24 +201,32 @@ void Game::loop()
       refractionFramebuffer.unbindToViewport(screenResolution.getWidth(), screenResolution.getHeight());
     }
 
-  //render our world onto separate FBO as usual
+  //render the whole scene onto appropriate FBO, depend on multisampling option
   bool multisamplingEnabled = options[OPT_USE_MULTISAMPLING];
   screenFramebuffer.bindAppropriateFBO(multisamplingEnabled);
   drawFrame(projectionView);
   screenFramebuffer.draw(multisamplingEnabled, options[OPT_USE_DOF]);
 
+  //save/load routines
   if (options[OPT_SAVE_REQUEST])
     saveState();
   if (options[OPT_LOAD_REQUEST])
     loadState();
 
+  //wait for buffer swapping
   {
     BENCHMARK("GLFW: swapBuffers", true);
     glfwSwapBuffers(window);
   }
+
+  //frame is complete
   ++updateCount;
 }
 
+/**
+* @brief prepares OpenGL state to new rendering cycle and manages rendering order of the "onscreen" elements (scene, gui etc.)
+* @param projectionView "projection * view" matrix
+*/
 void Game::drawFrame(const glm::mat4 &projectionView)
 {
   BENCHMARK("Game loop: draw frame", true);
@@ -165,8 +243,10 @@ void Game::drawFrame(const glm::mat4 &projectionView)
                   camera,
                   mouseInput);
 
-  //after all mesh related draw calls we could start updating meshes indirect data buffers
-  //start updating right after we've used it and before we need that data to be updated and buffered again
+  /*
+  * after all mesh related draw calls we could start updating meshes indirect data buffers
+  * start updating right after we've used it and before we need that data to be updated and buffered again
+  */
   modelsIndirectBufferNeedUpdate = true;
   modelsIndirectBufferNeedUpdateCV.notify_all();
 
@@ -180,6 +260,11 @@ void Game::drawFrame(const glm::mat4 &projectionView)
   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 
+/**
+* @brief manages frustum visualization routine
+* @param projectionView "projection * view" matrix
+* @todo remove this function from the release version of the game
+*/
 void Game::drawFrustumVisualizations(const glm::mat4& projectionView)
 {
   Shader& frustumShader = shaderManager.get(SHADER_FRUSTUM);
@@ -188,9 +273,9 @@ void Game::drawFrustumVisualizations(const glm::mat4& projectionView)
   if (options[OPT_FRUSTUM_VISUALIZATION])
     {
       frustumShader.setInt("u_colorIndex", 3);
-      shadowFrustumRenderers[0].render();
+      shadowRegionsFrustumsRenderers[0].render();
       frustumShader.setInt("u_colorIndex", 4);
-      shadowFrustumRenderers[1].render();
+      shadowRegionsFrustumsRenderers[1].render();
     }
   if (options[OPT_EXPECTED_VOLUME_VISUALIZATION])
     {
@@ -220,12 +305,17 @@ void Game::drawFrustumVisualizations(const glm::mat4& projectionView)
     }
 }
 
+/**
+* @brief manages OpenGL state to new world reflection rendering cycle and delegates draw reflection command to the scene
+* @note world reflection rendering require reflected view matrix
+*/
 void Game::drawFrameReflection()
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   if (options[OPT_USE_MULTISAMPLING])
     glDisable(GL_MULTISAMPLE);
 
+  //for reflection rendering we need reflection view matrix
   glm::mat4 viewReflected = camera.getReflectionViewMatrix();
   scene.drawWorldReflection(projection * viewReflected,
                             projection * glm::mat4(glm::mat3(viewReflected)),
@@ -236,6 +326,10 @@ void Game::drawFrameReflection()
     glEnable(GL_MULTISAMPLE);
 }
 
+/**
+* @brief manages OpenGL state to new world refraction rendering cycle and delegates draw refraction command to the scene
+* @param projectionView "projection * view" matrix
+*/
 void Game::drawFrameRefraction(const glm::mat4 &projectionView)
 {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -248,6 +342,9 @@ void Game::drawFrameRefraction(const glm::mat4 &projectionView)
     glEnable(GL_MULTISAMPLE);
 }
 
+/**
+* @brief handles recreation routine
+*/
 void Game::recreate()
 {
   BENCHMARK("Game loop: recreate", true);
@@ -255,30 +352,46 @@ void Game::recreate()
   options[OPT_RECREATE_TERRAIN_REQUEST] = false;
 }
 
-void Game::updateDepthmap()
+/**
+* @brief manages depthmap rendering routine: everything needed to add shadows to the frame is done here
+*/
+void Game::drawDepthmap()
 {
   glm::mat4 shadowView = shadowCamera.getViewMatrix();
+
+  //update shadow regions view frustums
   for (unsigned int layerIndex = 0; layerIndex < NUM_SHADOW_LAYERS; layerIndex++)
     {
-      shadowCameraFrustums[layerIndex].updateFrustum(shadowProjections[layerIndex] * shadowView);
-      shadowCameraFrustums[layerIndex].calculateIntersectionPoints();
+      shadowRegionsFrustums[layerIndex].updateFrustum(shadowRegionsProjections[layerIndex] * shadowView);
+      shadowRegionsFrustums[layerIndex].calculateIntersectionPoints();
     }
+
+  //update shadow volume
   {
     BENCHMARK("Shadow volume: update", true);
-    shadowVolume.update(shadowCameraFrustums, scene.getSunFacade());
+    shadowVolume.update(shadowRegionsFrustums, scene.getSunFacade());
   }
 
+  //draw scene onto depthmap
   depthmapFramebuffer.bindToViewport(DEPTH_MAP_TEXTURE_WIDTH, DEPTH_MAP_TEXTURE_HEIGHT);
   scene.drawWorldDepthmap(options[OPT_GRASS_SHADOW]);
   depthmapFramebuffer.unbindToViewport(screenResolution.getWidth(), screenResolution.getHeight());
 }
 
+/**
+* @brief handles file saving routine
+* @todo make proper saving system (with GUI, file naming stuff and other user-friendly bullshit)
+*/
 void Game::saveState()
 {
   saveLoadManager.saveToFile(SAVES_DIR + "testSave.txt");
   options[OPT_SAVE_REQUEST] = false;
 }
 
+/**
+* @brief handles file loading routine. Explicitly sends load command to scene as it should recalculate its internal data
+* @todo make proper loading system (with GUI, file naming stuff and other user-friendly bullshit)
+*/
 void Game::loadState()
 {
   saveLoadManager.loadFromFile(SAVES_DIR + "testSave.txt");
@@ -286,6 +399,10 @@ void Game::loadState()
   options[OPT_LOAD_REQUEST] = false;
 }
 
+/**
+* @brief initializes all game-coroutines threads
+* @note although there is only one coroutine thread at the moment, things might have changed in a future
+*/
 void Game::setupThreads()
 {
   meshIndirectBufferUpdater = std::make_unique<std::thread>([this]()
--- src/game/Game.h
+++ src/game/Game.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * Game.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains declaration for Game class
+ * @version 0.1.0
+ */
+
 #pragma once
 
 #include <GL/glew.h>
@@ -29,6 +49,10 @@
 class ScreenResolution;
 class MouseInputManager;
 
+/**
+* @brief entry point for game itself. Responsible for initialization/finalization of all submodules, threads communication,
+* updating events and game frame management
+*/
 class Game
 {
 public:
@@ -43,23 +67,39 @@ private:
   void drawFrameReflection();
   void drawFrameRefraction(const glm::mat4& projectionView);
   void recreate();
-  void updateDepthmap();
+  void drawDepthmap();
   void saveState();
   void loadState();
 
-  //context and hardware
+  //context and hardware related
+  /** 
+  * @note for now, screen resolution is supposed to be fixed during app workflow 
+  * @todo make it possible to change screen resolution "on the fly" (possibly with windowed mode option)
+  */
   const ScreenResolution& screenResolution;
   GLFWwindow* window;
+
+  //frame management
   Timer CPU_timer;
-  unsigned long updateCount = 0;
+  unsigned long updateCount;
 
   //camera and related stuff
-  Camera camera, shadowCamera;
-  Frustum viewFrustum, cullingViewFrustum;
-  std::array<Frustum, NUM_SHADOW_LAYERS> shadowCameraFrustums;
-  std::array<FrustumRenderer, NUM_SHADOW_LAYERS-1> shadowFrustumRenderers;
-  glm::mat4 projection, cullingProjection;
-  std::array<glm::mat4, NUM_SHADOW_LAYERS> shadowProjections;
+  Camera camera; 
+  /** 
+  * @brief additional camera defining shadow regions, intended to be used only for visual debugging
+  * @todo remove shadow camera in the release version of the game
+  */
+  Camera shadowCamera;
+  Frustum viewFrustum; 
+  /** @brief dedicated view frustum for hills used to perform custom frustum culling algorithm */
+  Frustum cullingViewFrustum;
+  std::array<Frustum, NUM_SHADOW_LAYERS> shadowRegionsFrustums;
+  /** @note don't need the farthest shadow region frustum visualization, thus size is 2 instead of 3 */
+  std::array<FrustumRenderer, NUM_SHADOW_LAYERS-1> shadowRegionsFrustumsRenderers;
+  glm::mat4 projection; 
+  /** @brief dedicated projection matrix used with culling frustum, has wider FOV */
+  glm::mat4 cullingProjection;
+  std::array<glm::mat4, NUM_SHADOW_LAYERS> shadowRegionsProjections;
 
   //options
   Options options;
@@ -77,6 +117,7 @@ private:
   //world
   ShadowVolume shadowVolume;
   Scene scene;
+  /** @todo remove this in the game release version*/
   ShadowVolumeRenderer shadowVolumeRenderer;
   SaveLoadManager saveLoadManager;
 
@@ -95,5 +136,6 @@ private:
   std::condition_variable modelsIndirectBufferNeedUpdateCV;
   std::atomic_bool modelsIndirectBufferPrepared, modelsIndirectBufferNeedUpdate;
 
-  bool landIndirectBufferHasUpdated = false; //not used in any other threads, thus no need to declare as atomic
+  /** @note not used in any other threads, thus no need to declare as atomic */
+  bool landIndirectBufferHasUpdated;
 };
