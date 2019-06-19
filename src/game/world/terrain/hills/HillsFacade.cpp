a93dfaf7101e67c86b10e9434ae7a0c3c138d9ba
ButtonHeck <soundmaker1505@gmail.com>
2019-06-19 19:33:37 +0300

    documentation for HillsFacade

--- src/game/world/terrain/hills/HillsFacade.cpp
+++ src/game/world/terrain/hills/HillsFacade.cpp
@@ -1,5 +1,32 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * HillsFacade.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definitions for HillsFacade class
+ * @version 0.1.0
+ */
+
 #include "HillsFacade"
 
+/**
+* @brief plain ctor. Constructs all the member subsystems.
+* @param renderShader shader program used during rendering (no frustum culling)
+* @param cullingShader shader program used during offscreen rendering with frustum culling
+* @param normalsShader shader program used during normals visualization rendering
+* @param waterMap map of the water tiles
+*/
 HillsFacade::HillsFacade(Shader &renderShader, Shader &cullingShader, Shader &normalsShader, const map2D_f &waterMap)
   :
     shaders(renderShader, cullingShader, normalsShader),
@@ -7,26 +34,53 @@ HillsFacade::HillsFacade(Shader &renderShader, Shader &cullingShader, Shader &no
     renderer(shaders, generator)
 {}
 
+/**
+* @brief sends setup command to generator
+*/
 void HillsFacade::setup()
 {
   generator.setup();
 }
 
+/**
+* @brief sends postprocess command to generator
+*/
 void HillsFacade::createTilesAndBufferData()
 {
   generator.createTilesAndBufferData();
 }
 
+/**
+* @brief delegates serialization call to generator
+* @param output file stream to write data to
+*/
 void HillsFacade::serialize(std::ofstream &output)
 {
   generator.serialize(output, true, 4);
 }
 
+/**
+* @brief delegates deserialization call to generator
+* @param input file stream to read data from
+*/
 void HillsFacade::deserialize(std::ifstream &input)
 {
   generator.deserialize(input);
 }
 
+/**
+* @brief handles hill drawing routine: prepares shader and delegates a draw command to renderer
+* @param lightDir sunlight direction vector
+* @param lightSpaceMatrices array of 3 "projection * view" matrices from the Sun point of view
+* @param projectionView "projection * view" matrix
+* @param viewPosition position vector of the camera
+* @param viewAcceleration a pair of camera's acceleration values (on X and Y axis), used for frustum culling
+* @param viewFrustum view frustum of the camera
+* @param useFrustumCulling indicator of whether to use frustum culling when drawing
+* @param useShadows indicator of whether shadows should be calculated
+* @param useDebugRender indicator of whether to draw additional details (mesh grid and normals), used for visual debug only
+* @todo remove debug render mode in the release version of the game
+*/
 void HillsFacade::draw(const glm::vec3 &lightDir,
                        const std::array<glm::mat4, NUM_SHADOW_LAYERS> &lightSpaceMatrices,
                        const glm::mat4& projectionView,
@@ -57,6 +111,9 @@ void HillsFacade::draw(const glm::vec3 &lightDir,
     }
 }
 
+/**
+* @brief delegates draw depthmap command to renderer
+*/
 void HillsFacade::drawDepthmap()
 {
   renderer.renderDepthmap();
--- src/game/world/terrain/hills/HillsFacade.h
+++ src/game/world/terrain/hills/HillsFacade.h
@@ -1,9 +1,32 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * HillsFacade.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains declaration for HillsFacade class
+ * @version 0.1.0
+ */
+
 #pragma once
 
 #include "HillsGenerator"
 #include "HillsShader"
 #include "HillsRenderer"
 
+/**
+* @brief facade for hills related code. Responsible for delegating tasks to its member objects
+*/
 class HillsFacade
 {
 public:
