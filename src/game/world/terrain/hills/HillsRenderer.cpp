67f1f130a1ce292f43a76052798d1a64cc58b49e
ButtonHeck <soundmaker1505@gmail.com>
2019-06-20 17:53:13 +0300

    documentation for HillsRenderer

--- src/game/world/terrain/hills/HillsRenderer.cpp
+++ src/game/world/terrain/hills/HillsRenderer.cpp
@@ -1,31 +1,70 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * HillsRenderer.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definitions for HillsRenderer class
+ * @version 0.1.0
+ */
+
 #include "HillsRenderer"
 #include "HillsGenerator"
 #include "HillsShader"
 #include "Shader"
 #include "BenchmarkTimer"
 
+/**
+* @brief plain ctor.
+* @param shaders shader manager
+* @param generator hills generator, used to fetch GL buffer objects for rendering
+*/
 HillsRenderer::HillsRenderer(HillsShader &shaders, HillsGenerator &generator) noexcept
   :
     shaders(shaders),
     generator(generator)
 {}
 
+/**
+* @brief manages draw calls to OpenGL and optionally switches to custom frustum culling mode rendering
+* @param useFrustumCulling indicator of frustum culling mode
+* @param viewAcceleration a pair of camera's acceleration values (on X and Y axis), used for frustum culling 
+*/
 void HillsRenderer::render(bool useFrustumCulling, const glm::vec2& viewAcceleration)
 {
   if (useFrustumCulling)
     {
+	  /*
+	  * by default transform feedback rendering takes place every FEEDBACK_UPDATE_FRAMERATE frames,
+	  * but the more acceleration values of the camera are, the more frequent feedback rendering should occur.
+	  * Thus, adjust framerate of feedback rendering depend on camera's current acceleration (max X or Y)
+	  */
+	  const unsigned int FEEDBACK_UPDATE_FRAMERATE = 12;
       float accelerationAbs = glm::max(glm::abs(viewAcceleration.x), glm::abs(viewAcceleration.y));
-      int updateOffset = 1;
+      int updateIncrement = 1;
       if (accelerationAbs > 0.5f && accelerationAbs <= 5.0f)
-        updateOffset = 4;
+        updateIncrement = 4;
       else if (accelerationAbs > 5.0f && accelerationAbs <= 10.0f)
-        updateOffset = 5;
+        updateIncrement = 5;
       else if (accelerationAbs > 10.0f)
-        updateOffset = 6;
+        updateIncrement = 6;
+
+	  //made static in order to keep this counter value from call to call
       static unsigned int updateTransformFeedbackCounter = 0;
       GLuint transformFeedback = generator.culledBuffers.get(TFBO);
       if (updateTransformFeedbackCounter == 0)
         {
+		  //render hills offscreen with transform feedback
           BENCHMARK("HillsRenderer: draw to TFB", true);
           shaders.cullingShader.use();
           generator.basicGLBuffers.bind(VAO);
@@ -36,10 +75,11 @@ void HillsRenderer::render(bool useFrustumCulling, const glm::vec2& viewAccelera
           glEndTransformFeedback();
           glDisable(GL_RASTERIZER_DISCARD);
         }
-      updateTransformFeedbackCounter += updateOffset;
-      if (updateTransformFeedbackCounter >= FEEDBACK_UPDATE_DELAY)
+      updateTransformFeedbackCounter += updateIncrement;
+      if (updateTransformFeedbackCounter >= FEEDBACK_UPDATE_FRAMERATE)
         updateTransformFeedbackCounter = 0;
       {
+		//render feedback onscreen
         BENCHMARK("HillsRenderer: draw from TFB", true);
         shaders.renderShader.use();
         generator.culledBuffers.bind(VAO);
@@ -48,6 +88,7 @@ void HillsRenderer::render(bool useFrustumCulling, const glm::vec2& viewAccelera
     }
   else
     {
+	  //plain onscreen rendering
       BENCHMARK("HillsRenderer: draw", true);
       shaders.renderShader.use();
       generator.basicGLBuffers.bind(VAO);
@@ -55,6 +96,9 @@ void HillsRenderer::render(bool useFrustumCulling, const glm::vec2& viewAccelera
     }
 }
 
+/**
+* @brief render hills to depthmap
+*/
 void HillsRenderer::renderDepthmap()
 {
   BENCHMARK("HillsRenderer: render depthmap", true);
@@ -62,6 +106,11 @@ void HillsRenderer::renderDepthmap()
   glDrawElements(GL_TRIANGLES, generator.tiles.size() * VERTICES_PER_QUAD, GL_UNSIGNED_INT, 0);
 }
 
+/**
+* @brief utulity function that renders additional visual information such as normals/tangents/bitangents, hills grid etc.
+* @param primitiveType GL enumerated value of a geometry primitive to use for rendering
+* @todo remove this function in release version of the game
+*/
 void HillsRenderer::debugRender(GLenum primitiveType)
 {
   generator.basicGLBuffers.bind(VAO);
--- src/game/world/terrain/hills/HillsRenderer.h
+++ src/game/world/terrain/hills/HillsRenderer.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * HillsRenderer.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains declaration for HillsRenderer class
+ * @version 0.1.0
+ */
+
 #pragma once
 
 #include <GL/glew.h>
@@ -6,6 +26,9 @@
 class HillsGenerator;
 class HillsShader;
 
+/**
+* @brief Renderer for hills. Responsible both for onscreen and offscreen (for frustum culling) rendering
+*/
 class HillsRenderer
 {
 public:
@@ -15,7 +38,6 @@ public:
   void debugRender(GLenum primitiveType);
 
 private:
-  const unsigned int FEEDBACK_UPDATE_DELAY = 12;
   HillsShader& shaders;
   HillsGenerator& generator;
 };
