3ae3d627652d95dd17f0699eb98b104c6b87cb4a
ButtonHeck <soundmaker1505@gmail.com>
2019-06-21 18:53:19 +0300

    documentation for LandGenerator

--- src/game/world/terrain/land/LandGenerator.cpp
+++ src/game/world/terrain/land/LandGenerator.cpp
@@ -1,7 +1,30 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * LandGenerator.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definitions for LandGenerator class
+ * @version 0.1.0
+ */
+
 #include "LandGenerator"
 
 #include <chrono>
 
+/**
+* @brief plain ctor. Explicitly initializes cells buffer collection for indirect buffer usage, initializes randomizer seed
+*/
 LandGenerator::LandGenerator() noexcept
   :
     Generator(),
@@ -12,6 +35,10 @@ LandGenerator::LandGenerator() noexcept
   basicGLBuffers.add(INSTANCE_VBO);
 }
 
+/**
+* @brief prepares land map and buffer collections
+* @param shoreMap map of the shore tiles
+*/
 void LandGenerator::setup(const map2D_f &shoreMap)
 {
   initializeMap(chunkMap);
@@ -23,6 +50,10 @@ void LandGenerator::setup(const map2D_f &shoreMap)
   fillCellBufferData();
 }
 
+/**
+* @brief copies data from the shore map
+* @param shoreMap map of the shore tiles
+*/
 void LandGenerator::generateMap(const map2D_f &shoreMap)
 {
   for (unsigned int y = 0; y <= WORLD_HEIGHT; y++)
@@ -32,8 +63,13 @@ void LandGenerator::generateMap(const map2D_f &shoreMap)
     }
 }
 
+/**
+* @brief setup square chunks storage
+* @param chunkSize size of one square chunk
+*/
 void LandGenerator::splitChunks(int chunkSize)
 {
+  //in case of recreation need to remove old data
   chunks.clear();
   tiles.clear();
   unsigned int chunkOffset = 0;
@@ -41,21 +77,22 @@ void LandGenerator::splitChunks(int chunkSize)
     {
       for (int startX = 0; startX < WORLD_WIDTH - chunkSize + 1; startX += chunkSize)
         {
-          bool emptyChunk = true;
+		  //check if region of the map is flat
+          bool chunkIsFlat = true;		  
           for (int y = startY; y < startY + chunkSize; y++)
             {
               for (int x = startX; x < startX + chunkSize; x++)
                 {
                   if (map[y][x] != 0 || map[y+1][x] != 0 || map[y+1][x+1] != 0 || map[y][x+1] != 0)
                     {
-                      emptyChunk = false;
+                      chunkIsFlat = false;
                       break;
                     }
                 }
-              if (!emptyChunk)
+              if (!chunkIsFlat)
                 break;
             }
-          if (emptyChunk)
+          if (chunkIsFlat)
             {
               for (int noRenderY = startY + 1; noRenderY < startY + chunkSize; noRenderY++)
                 {
@@ -65,6 +102,7 @@ void LandGenerator::splitChunks(int chunkSize)
                       chunkMap[noRenderY][noRenderX] = CHUNK_NO_RENDER_VALUE;
                     }
                 }
+			  //for square map segments one tile is also one chunk
               tiles.emplace_back(startX, startY, 0, 0, 0, 0);
               chunks.emplace_back(startX, startX + chunkSize, startY, startY + chunkSize, chunkOffset, 1);
               ++chunkOffset;
@@ -73,8 +111,13 @@ void LandGenerator::splitChunks(int chunkSize)
     }
 }
 
+/**
+* @brief setup cells chunks storage
+* @param chunkSize size of one cells chunk
+*/
 void LandGenerator::splitCellChunks(int chunkSize)
 {
+  //in case of recreation need to remove old data
   cellTiles.clear();
   cellChunks.clear();
   unsigned int chunkOffset = 0;
@@ -87,6 +130,7 @@ void LandGenerator::splitCellChunks(int chunkSize)
             {
               for (int x = startX; x < startX + chunkSize; x++)
                 {
+				  //check if this segment is flat and has not been used in square chunk storage
                   if (map[y][x] == 0 && map[y-1][x] == 0 && map[y-1][x+1] == 0 && map[y][x+1] == 0 &&
                       chunkMap[y][x] != CHUNK_NO_RENDER_VALUE &&
                       chunkMap[y-1][x] != CHUNK_NO_RENDER_VALUE &&
@@ -105,8 +149,12 @@ void LandGenerator::splitCellChunks(int chunkSize)
     }
 }
 
+/**
+* @brief prepares square segments buffer collection
+*/
 void LandGenerator::fillBufferData()
 {
+  //buffer data for exactly one square chunk
   float halfChunkSize = CHUNK_SIZE / 2;
   const unsigned int CHUNK_VERTICES_SIZE_FLOATS = 20;
   GLfloat chunkVertices[CHUNK_VERTICES_SIZE_FLOATS] = {
@@ -117,8 +165,9 @@ void LandGenerator::fillBufferData()
   };
   basicGLBuffers.bind(VAO);
   bufferData(basicGLBuffers, chunkVertices, CHUNK_VERTICES_SIZE_FLOATS);
-  setupGLBufferAttributes();
+  setupVBOAttributes();
 
+  //buffer instances translations data to instance vbo
   std::unique_ptr<glm::vec4[]> landChunkInstanceTranslations(new glm::vec4[tiles.size()]);
   for (unsigned int tileIndex = 0; tileIndex < tiles.size(); tileIndex++)
     {
@@ -130,12 +179,16 @@ void LandGenerator::fillBufferData()
     }
   basicGLBuffers.bind(INSTANCE_VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec4) * tiles.size(), &landChunkInstanceTranslations[0], GL_STATIC_DRAW);
-  setupGLBufferInstancedAttributes();
+  setupVBOInstancedAttributes();
   BufferCollection::bindZero(VAO | VBO | EBO);
 }
 
+/**
+* @brief prepares cells chunks buffer collection
+*/
 void LandGenerator::fillCellBufferData()
 {
+  //buffer data for exactly one cell
   const unsigned int CELL_VERTICES_SIZE_FLOATS = 20;
   GLfloat cellVertices[CELL_VERTICES_SIZE_FLOATS] = {
        0.0f, 0.0f,  1.0f, 0.0f,  0.0f,
@@ -145,8 +198,9 @@ void LandGenerator::fillCellBufferData()
   };
   cellBuffers.bind(VAO | DIBO);
   bufferData(cellBuffers, cellVertices, CELL_VERTICES_SIZE_FLOATS);
-  setupGLBufferAttributes();
+  setupVBOAttributes();
 
+  //buffer cells instances data to instance vbo
   std::unique_ptr<glm::vec4[]> cellInstanceTranslations(new glm::vec4[cellTiles.size()]);
   for (unsigned int tileIndex = 0; tileIndex < cellTiles.size(); tileIndex++)
     {
@@ -155,10 +209,16 @@ void LandGenerator::fillCellBufferData()
     }
   cellBuffers.bind(INSTANCE_VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec4) * cellTiles.size(), &cellInstanceTranslations[0], GL_STATIC_DRAW);
-  setupGLBufferInstancedAttributes();
+  setupVBOInstancedAttributes();
   BufferCollection::bindZero(VAO | VBO | EBO);
 }
 
+/**
+* @brief helper function to load data from given buffer to buffer collection
+* @param bufferCollection buffer collection to load data to 
+* @param buffer client buffer storage
+* @param size number of elements to load
+*/
 void LandGenerator::bufferData(BufferCollection& bufferCollection, GLfloat* buffer, size_t size)
 {
   bufferCollection.bind(VBO | EBO);
@@ -166,7 +226,10 @@ void LandGenerator::bufferData(BufferCollection& bufferCollection, GLfloat* buff
   glBufferData(GL_ARRAY_BUFFER, sizeof(buffer) * size, buffer, GL_STATIC_DRAW);
 }
 
-void LandGenerator::setupGLBufferAttributes() noexcept
+/**
+* @brief helper function to setup vbo attributes and pointers
+*/
+void LandGenerator::setupVBOAttributes() noexcept
 {
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
@@ -174,13 +237,20 @@ void LandGenerator::setupGLBufferAttributes() noexcept
   glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
 }
 
-void LandGenerator::setupGLBufferInstancedAttributes() noexcept
+/**
+* @brief helper function to setup instance vbo attribute, pointer and divisor
+*/
+void LandGenerator::setupVBOInstancedAttributes() noexcept
 {
   glEnableVertexAttribArray(2);
   glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(glm::vec4), 0);
   glVertexAttribDivisor(2, 1);
 }
 
+/**
+* @brief updates indirect buffer data for cells storage
+* @param frustum camera's view frustum
+*/
 void LandGenerator::updateCellsIndirectBuffer(const Frustum& frustum)
 {
   cellBuffers.bind(VAO);
@@ -200,6 +270,13 @@ void LandGenerator::updateCellsIndirectBuffer(const Frustum& frustum)
   glBufferData(GL_DRAW_INDIRECT_BUFFER, INDIRECT_DRAW_COMMAND_BYTE_SIZE * cellPrimitiveCount, &indirectBuffer[0], GL_STATIC_DRAW);
 }
 
+/**
+* @brief load data of one inderect draw command to indirect buffer
+* @param buffer client buffer storage
+* @param dataOffset offset to storage index
+* @param numInstances number of instances for draw call
+* @param instanceOffset instance offset for draw call
+*/
 void LandGenerator::addIndirectBufferData(GLuint *buffer, GLuint &dataOffset, GLuint numInstances, GLuint instanceOffset) noexcept
 {
   buffer[dataOffset++] = VERTICES_PER_QUAD;
--- src/game/world/terrain/land/LandGenerator.h
+++ src/game/world/terrain/land/LandGenerator.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * LandGenerator.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains declaration for LandGenerator class
+ * @version 0.1.0
+ */
+
 #pragma once
 
 #include "Generator"
@@ -5,6 +25,10 @@
 
 #include <random>
 
+/**
+* @brief Generator for land terrain data. Has two types of storages: square tile chunks (that are not cut off by shore)
+* and cells chunks (non-square regions nearby shore). 
+*/
 class LandGenerator : public Generator
 {
 public:
@@ -23,8 +47,8 @@ private:
   void fillBufferData();
   void fillCellBufferData();
   void bufferData(BufferCollection &bufferCollection, GLfloat *buffer, size_t size);
-  void setupGLBufferAttributes() noexcept;
-  void setupGLBufferInstancedAttributes() noexcept;
+  void setupVBOAttributes() noexcept;
+  void setupVBOInstancedAttributes() noexcept;
   void addIndirectBufferData(GLuint* buffer, GLuint& dataOffset, GLuint numInstances, GLuint instanceOffset) noexcept;
 
   BufferCollection cellBuffers;
