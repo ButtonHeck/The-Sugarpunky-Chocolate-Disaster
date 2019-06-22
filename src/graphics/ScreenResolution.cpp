30d91c272092fe666f02d4c51022a786feabc2b1
ButtonHeck <soundmaker1505@gmail.com>
2019-06-22 20:55:46 +0300

    documentation and refactoring for ScreenResolution

--- src/graphics/ScreenResolution.cpp
+++ src/graphics/ScreenResolution.cpp
@@ -1,5 +1,30 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * ScreenResolution.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definitions for ScreenResolution class
+ * @version 0.1.0
+ */
+
 #include "ScreenResolution"
 
+/**
+* @brief plain ctor
+* @param width width of resolution
+* @param height height of resolution
+*/
 ScreenResolution::ScreenResolution(int width, int height) noexcept
   :
     width(width),
@@ -7,11 +32,16 @@ ScreenResolution::ScreenResolution(int width, int height) noexcept
     aspectRatio((float)width / (float)height)
 {}
 
+/**
+* @brief updates resolution dimensions and updates aspect ratio
+* @param newWidth new width of resolution
+* @param newHeight new height of resolution
+*/
 void ScreenResolution::updateResolution(int newWidth, int newHeight) noexcept
 {
   width = newWidth;
   height = newHeight;
-  updateAspectRatio((float)newWidth / (float)newHeight);
+  aspectRatio = (float)newWidth / (float)newHeight;
 }
 
 int ScreenResolution::getWidth() const noexcept
@@ -29,17 +59,22 @@ float ScreenResolution::getAspectRatio() const noexcept
   return aspectRatio;
 }
 
+/**
+* @brief utility function needed for objects that are rendered as points whose size should be proportional
+* to actual screen resolution since the point size is absolute value, e.g. on larger monitors point objects should be
+* scaled according to that monitor resolution
+*/
 float ScreenResolution::getWidthRatioToReference() const noexcept
 {
   return width / REFERENCE_WIDTH;
 }
 
+/**
+* @brief utility function needed for objects that are rendered as points whose size should be proportional
+* to actual screen resolution since the point size is absolute value, e.g. on larger monitors point objects should be
+* scaled according to that monitor resolution
+*/
 float ScreenResolution::getHeightRatioToReference() const noexcept
 {
   return height / REFERENCE_HEIGHT;
-}
-
-void ScreenResolution::updateAspectRatio(float newAspect) noexcept
-{
-  aspectRatio = newAspect;
 }
--- src/graphics/ScreenResolution.h
+++ src/graphics/ScreenResolution.h
@@ -1,12 +1,35 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * ScreenResolution.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains declaration for ScreenResolution class
+ * @version 0.1.0
+ */
+
 #pragma once
 
+/**
+* @brief Utility class representing screen resolution. 
+* Responsible for holding resolution parameters: width, height and ratio
+*/
 class ScreenResolution
 {
 public:
   constexpr static float REFERENCE_WIDTH = 1440.0f;
   constexpr static float REFERENCE_HEIGHT = 900.0f;
 
-  ScreenResolution() = default;
   ScreenResolution(int width, int height) noexcept;
   void updateResolution(int newWidth, int newHeight) noexcept;
   int getWidth() const noexcept;
@@ -16,8 +39,6 @@ public:
   float getHeightRatioToReference() const noexcept;
 
 private:
-  void updateAspectRatio(float newAspect) noexcept;
-
-  int width = 0, height = 0;
-  float aspectRatio = 1.0f;
+  int width, height;
+  float aspectRatio;
 };
