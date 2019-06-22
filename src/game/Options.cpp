dfb6023494b6fb43f998b0ee66df10a31807f516
ButtonHeck <soundmaker1505@gmail.com>
2019-06-22 14:39:35 +0300

    documentation for Options

--- src/game/Options.cpp
+++ src/game/Options.cpp
@@ -1,5 +1,28 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * Options.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definitions for Options class
+ * @version 0.1.0
+ */
+
 #include "Options"
 
+/**
+* @brief plain ctor, initializes all options to appropriate default values
+*/
 Options::Options() noexcept
 {
   options[OPT_DRAW_TREES] = true;
@@ -31,13 +54,20 @@ Options::Options() noexcept
   options[OPT_SHOW_VRAM_AVAILABLE] = false;
 }
 
-std::bitset<OPTIONS_COUNT>::reference Options::operator[](OPTION value)
+/**
+* @brief utility operator overloading that just returns bool value for a given option
+* @param option option to check
+*/
+std::bitset<OPTIONS_COUNT>::reference Options::operator[](OPTION option)
 {
-  return options[value];
+  return options[option];
 }
 
-void Options::toggle(OPTION value)
+/**
+* @brief switches option state
+* @param option option to switch
+*/
+void Options::toggle(OPTION option)
 {
-  options.flip(value);
+  options.flip(option);
 }
-
--- src/game/Options.h
+++ src/game/Options.h
@@ -1,15 +1,38 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * Options.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains declaration for Options class
+ * @version 0.1.0
+ */
+
 #pragma once
 
 #include "OptionsUnits"
 
 #include <bitset>
 
+/**
+* @brief Represents simple wrapper for bitset of option units. Responsible for managing in-game options state
+*/
 class Options
 {
 public:
   Options() noexcept;
-  std::bitset<OPTIONS_COUNT>::reference operator[](OPTION value);
-  void toggle(OPTION value);
+  std::bitset<OPTIONS_COUNT>::reference operator[](OPTION option);
+  void toggle(OPTION option);
 
 private:
   std::bitset<OPTIONS_COUNT> options;
