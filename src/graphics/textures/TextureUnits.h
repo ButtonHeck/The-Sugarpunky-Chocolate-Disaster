f89c35850a606ee871b9a741f5b6125cb12252eb
ButtonHeck <soundmaker1505@gmail.com>
2019-06-23 21:43:00 +0300

    documentation for TextureUnits

--- src/graphics/textures/TextureUnits.h
+++ src/graphics/textures/TextureUnits.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * TextureUnits.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains enumeration of all texture units
+ * @version 0.1.0
+ */
+
 #pragma once
 
 enum TEXTURE_UNIT
