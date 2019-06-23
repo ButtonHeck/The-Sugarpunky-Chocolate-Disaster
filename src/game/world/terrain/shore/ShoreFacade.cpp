f3bd6f49989cbe7fb1f97da1f292ba01eb65b1ac
ButtonHeck <soundmaker1505@gmail.com>
2019-06-23 14:27:04 +0300

    documentation for ShoreFacade

--- src/game/world/terrain/shore/ShoreFacade.cpp
+++ src/game/world/terrain/shore/ShoreFacade.cpp
@@ -1,6 +1,32 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * ShoreFacade.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definitions for ShoreFacade class
+ * @version 0.1.0
+ */
+
 #include "ShoreFacade"
 #include "BenchmarkTimer"
 
+/**
+* @brief plain ctor. Creates all member submodules
+* @param renderShader shader program used for onscreen rendering
+* @param normalsShader shader program used for onscreen normals rendering
+* @param waterMap map of the water
+*/
 ShoreFacade::ShoreFacade(Shader &renderShader, Shader &normalsShader, const map2D_f &waterMap)
   :
     shader(renderShader, normalsShader),
@@ -8,21 +34,42 @@ ShoreFacade::ShoreFacade(Shader &renderShader, Shader &normalsShader, const map2
     renderer(generator)
 {}
 
+/**
+* @brief delegates map generation routine to generator
+*/
 void ShoreFacade::setup()
 {
   generator.setup();
 }
 
+/**
+* @brief delegates serialization call to generator
+* @param output file stream to write data to
+*/
 void ShoreFacade::serialize(std::ofstream &output)
 {
   generator.serialize(output);
 }
 
+/**
+* @brief delegates deserialization call to generator
+* @param input file stream to read data from
+*/
 void ShoreFacade::deserialize(std::ifstream &input)
 {
   generator.deserialize(input);
 }
 
+/**
+* @brief handles shores drawing routine: prepares shader and delegates draw command to renderer
+* @param lightDir sunlight direction vector
+* @param lightSpaceMatrices array of "projection * view" matrices from the Sun point of view
+* @param projectionView "projection * view" matrix
+* @param useShadows indicator of whether shadows should be calculated
+* @param useDebugRender indicator of whether to render additional visualizations for debugging
+* @param useClipDistanceReflection indicator of whether clip distance will be used by OpenGL for reflection rendering
+* @param useClipDistanceRefraction indicator of whether clip distance will be used by OpenGL for refraction rendering
+*/
 void ShoreFacade::draw(const glm::vec3 &lightDir,
                        const std::array<glm::mat4, NUM_SHADOW_LAYERS> &lightSpaceMatrices,
                        const glm::mat4 &projectionView,
@@ -50,12 +97,6 @@ void ShoreFacade::draw(const glm::vec3 &lightDir,
       shader.updateNormals(projectionView);
       renderer.debugRender(GL_POINTS);
     }
-}
-
-void ShoreFacade::drawDepthmap()
-{
-  BENCHMARK("ShoreRenderer: draw depthmap", true);
-  renderer.render();
 }
 
 const map2D_f &ShoreFacade::getMap() const noexcept
--- src/game/world/terrain/shore/ShoreFacade.h
+++ src/game/world/terrain/shore/ShoreFacade.h
@@ -1,9 +1,32 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * ShoreFacade.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains declaration for ShoreFacade class
+ * @version 0.1.0
+ */
+
 #pragma once
 
 #include "ShoreGenerator"
 #include "ShoreShader"
 #include "ShoreRenderer"
 
+/**
+* @brief facade for shore related code. Responsible for delegating tasks to member objects
+*/
 class ShoreFacade
 {
 public:
@@ -18,7 +41,6 @@ public:
             bool useDebugRender,
             bool useClipDistanceReflection,
             bool useClipDistanceRefraction);
-  void drawDepthmap();
   const map2D_f& getMap() const noexcept;
 
 private:
