eb5518ed2c3ac0d5822b146095021ff2c62dc3fc
ButtonHeck <soundmaker1505@gmail.com>
2019-06-24 00:20:24 +0300

    documentation for VRAM_Monitor

--- src/util/VRAM_Monitor.cpp
+++ src/util/VRAM_Monitor.cpp
@@ -1,18 +1,47 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * VRAM_Monitor.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definitions for VRAM_Monitor class
+ * @version 0.1.0
+ */
+
 #include "VRAM_Monitor"
 #include <GL/glew.h>
 
+/**
+* @brief initializes member variables by requesting data from OpenGL directly
+*/
 VRAM_Monitor::VRAM_Monitor() noexcept
 {
   glGetIntegerv(GL_GPU_MEMORY_INFO_TOTAL_AVAILABLE_MEMORY_NVX, &ramSize);
   ramSizeFloatPercentage = (float)ramSize / 100;
 }
 
+/**
+* @brief returns instance of a monitor
+*/
 VRAM_Monitor &VRAM_Monitor::getInstance() noexcept
 {
   static VRAM_Monitor instance;
   return instance;
 }
 
+/**
+* @brief sends get integer value call to OpenGL to update current available memory size info
+*/
 void VRAM_Monitor::updateAvailable() noexcept
 {
   glGetIntegerv(GL_GPU_MEMORY_INFO_CURRENT_AVAILABLE_VIDMEM_NVX, &ramAvailable);
@@ -23,6 +52,9 @@ int VRAM_Monitor::getAvailableMemory() const noexcept
   return ramAvailable;
 }
 
+/**
+* @brief returns percent value of available memory
+*/
 float VRAM_Monitor::getAvailableMemoryPercent() const noexcept
 {
   return ramAvailable / ramSizeFloatPercentage;
--- src/util/VRAM_Monitor.h
+++ src/util/VRAM_Monitor.h
@@ -1,5 +1,28 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * VRAM_Monitor.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains declaration for VRAM_Monitor class
+ * @version 0.1.0
+ */
+
 #pragma once
 
+/**
+* @brief utility class responsible for updating available VRAM information
+*/
 class VRAM_Monitor final
 {
 public:
@@ -10,6 +33,7 @@ public:
 
 private:
   VRAM_Monitor() noexcept;
+
   int ramAvailable, ramSize;
   float ramSizeFloatPercentage;
 };
