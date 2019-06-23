82f9bc291521f2532ebeb883ec07b008d696f1f8
ButtonHeck <soundmaker1505@gmail.com>
2019-06-23 15:33:34 +0300

    documentation for ShoreRenderer

--- src/game/world/terrain/shore/ShoreRenderer.cpp
+++ src/game/world/terrain/shore/ShoreRenderer.cpp
@@ -1,11 +1,38 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * ShoreRenderer.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definitions for ShoreRenderer class
+ * @version 0.1.0
+ */
+
 #include "ShoreRenderer"
 #include "ShoreGenerator"
 
+/**
+* @brief plain ctor
+* @param generator generator object to fetch vao from
+*/
 ShoreRenderer::ShoreRenderer(ShoreGenerator &generator) noexcept
   :
     generator(generator)
 {}
 
+/**
+* @brief sends draw call to OpenGL with blending enabled
+*/
 void ShoreRenderer::render()
 {
   generator.basicGLBuffers.bind(VAO);
@@ -14,6 +41,11 @@ void ShoreRenderer::render()
   glDisable(GL_BLEND);
 }
 
+/**
+* @brief sends draw call to OpenGL to draw additional visualizations for debugging
+* @param primitiveType GL defined primitive type to render
+* @todo remove this in release version of the game
+*/
 void ShoreRenderer::debugRender(GLenum primitiveType)
 {
   generator.basicGLBuffers.bind(VAO);
