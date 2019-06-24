b89c8d17da8ed7c0a417943dfbbe6ea2b41a21fa
ButtonHeck <soundmaker1505@gmail.com>
2019-06-24 21:48:58 +0300

    documentation for WaterRefractionFramebuffer

--- src/graphics/framebuffers/WaterRefractionFramebuffer.cpp
+++ src/graphics/framebuffers/WaterRefractionFramebuffer.cpp
@@ -1,11 +1,38 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * WaterRefractionFramebuffer.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains definitions for WaterRefractionFramebuffer class
+ * @version 0.1.0
+ */
+
 #include "WaterRefractionFramebuffer"
 #include "TextureManager"
 
+/**
+* @brief plain ctor
+* @param textureManager texture manager
+*/
 WaterRefractionFramebuffer::WaterRefractionFramebuffer(TextureManager &textureManager) noexcept
   :
     Framebuffer(textureManager)
 {}
 
+/**
+* @brief initializes framebuffer to valid state
+*/
 void WaterRefractionFramebuffer::setup()
 {
   glBindFramebuffer(GL_FRAMEBUFFER, fbo);
--- src/graphics/framebuffers/WaterRefractionFramebuffer.h
+++ src/graphics/framebuffers/WaterRefractionFramebuffer.h
@@ -1,11 +1,34 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * WaterRefractionFramebuffer.h
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains declaration for WaterRefractionFramebuffer class
+ * @version 0.1.0
+ */
+
 #pragma once
 
 #include "Framebuffer"
 
+/**
+* @brief framebuffer for world refraction image rendering from water point of view
+* @note for depth component this framebuffer uses texture which is used in the water shader
+*/
 class WaterRefractionFramebuffer : public Framebuffer
 {
 public:
   WaterRefractionFramebuffer(TextureManager& textureManager) noexcept;
-  virtual ~WaterRefractionFramebuffer() = default;
   virtual void setup() override;
 };
