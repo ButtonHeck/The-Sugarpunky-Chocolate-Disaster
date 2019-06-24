7a2a3b33a47b76cbc55e7a2ce2e36d565f6b711f
ButtonHeck <soundmaker1505@gmail.com>
2019-06-24 22:50:12 +0300

    documentation for main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2019 Ilya Malgin
+ * main.cpp
+ * This file is part of The Sugarpunky Chocolate Disaster project
+ *
+ * The Sugarpunky Chocolate Disaster project is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Sugarpunky Chocolate Disaster project is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * See <http://www.gnu.org/licenses/>
+ *
+ * Purpose: contains application entry point code and main() function
+ * @version 0.1.0
+ */
+
 #include "Game"
 #include "ScreenResolution"
 #include "Logger"
@@ -5,10 +25,15 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+/**
+* @brief temporary global variable for debugging purposes
+* @todo remove this from release version of the game and put it in the Sun submodule instead
+*/
 float debug_sunSpeed = 2.0f;
 
 int main()
 {
+  //initialize GLFW stuff
   glfwSetErrorCallback([](int, const char* msg)
   {
       Logger::log("Error with GLFW library: %\n", msg);
@@ -26,6 +51,8 @@ int main()
   ScreenResolution screenResolution(vidmode->width, vidmode->height);
   GLFWwindow* window = glfwCreateWindow(screenResolution.getWidth(), screenResolution.getHeight(), "The Sugarpunky Chocolate Disaster", monitor, 0);
   glfwMakeContextCurrent(window);
+
+  //initialize GLEW
   glewExperimental = GL_TRUE;
   glewInit();
 
@@ -42,11 +69,17 @@ int main()
   }
 #endif
 
+  //explicitly make non-current from this thread, as the context will mainly be used in the game child thread
   glfwMakeContextCurrent(NULL);
 
   std::thread gameThread([&]()
   {
 	  glfwMakeContextCurrent(window);
+	  /*
+	   * compiler gives warning if I try to allocate Game object in stack memory,
+	   * on the other hand, using smart pointer for this only because of warning is overkill,
+	   * so, plain old new/delete is ok here
+	   */
 	  Game* game = new Game(window, screenResolution);
 	  game->setup();
 	  while (!glfwWindowShouldClose(window))
@@ -56,6 +89,7 @@ int main()
 	  delete game;
   });
 
+  //don't know why pollEvents function is working as we nullified current context for this thread, but it works.
   while (!glfwWindowShouldClose(window))
   {
 	  glfwPollEvents();
